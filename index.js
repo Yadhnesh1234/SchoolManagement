425174956bb99eb278c68e4d8ee8b76f7ea1e7de
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2025-03-27 12:46:06 +0530

    Deploy Issue Resolved

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(body_parser.json())
 
 app.use('/api/schools', schoolRoutes);
 
-const port = process.env.PORT | 3000
-app.listen(process.env.PORT,()=>{console.log(`Server Started at ${port}`)})
+// const port = process.env.PORT | 3000
+// app.listen(process.env.PORT,()=>{console.log(`Server Started at ${port}`)})
 
 module.exports = app;
